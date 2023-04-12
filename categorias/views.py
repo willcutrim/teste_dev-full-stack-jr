d4a647446f96a3fa010e24d5efd051be7e9be3ba
Willyam Cutrim <37386568+willcutrim@users.noreply.github.com>
2023-04-12 20:56:20 -0300

    Update views.py

--- categorias/views.py
+++ categorias/views.py
@@ -16,3 +16,21 @@ class CategoriaList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class CategoriaInfo(APIView):
+    def get_categoria(self, id):
+        try:
+            return Categoria.objects.get(id=id)
+        except Categoria.DoesNotExist:
+            raise Http404
+
+    def get(self, request, id):
+        categoria_id = self.get_categoria(id)
+        serializer = CategoriaSerializer(categoria_id)
+        return Response(serializer.data)
+    
+    def delete(self, request, id):
+        categoria = self.get_categoria(id)
+        categoria.delete()
+        message = {'message': 'cattegoria deletado com sucesso!'}
+        return Response(message, status=status.HTTP_204_NO_CONTENT)
