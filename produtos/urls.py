01f9b9d5b4d38db82df62243e3dc8fafcdc3b768
Willyam Cutrim <37386568+willcutrim@users.noreply.github.com>
2023-04-12 15:47:57 -0300

    Update urls.py

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProdutoList
+from .views import ProdutoList, ProdutoInfo
 
 urlpatterns = [
     path('', ProdutoList.as_view()),
+    path('<int:id>', ProdutoInfo.as_view()),
 ]
